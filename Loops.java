6e21c739cc6096d90abac7c28a680a6bbbc6109f
jeff1haupt <66330433+jeff1haupt@users.noreply.github.com>
2020-06-13 19:01:27 -0400

    Add files via upload

--- Loops.java
+++ Loops.java
@@ -11,6 +11,38 @@ public class Loops {
 			}
 			i++;
 		}
+		
+		System.out.println("\n\n-------------\n\n");
+		
+		int j = 100;
+		while(j > 0) {
+			System.out.println(j);
+			j = j - 3;
+		}
+		
+		System.out.println("\n\n-------------\n\n");
+		
+		for(int k=1; k < 100; k=k+2) {
+			System.out.println(k);
+		}
+		
+		System.out.println("\n\n-------------\n\n");
+		
+		for(int t=0; t < 100; t++) {
+			if(t % 3 == 0) {
+				if(t % 5 == 0) {
+					System.out.println("Hello World");
+				} else {
+					System.out.println("Hello");
+				}
+			} else if(t % 5 == 0){
+				System.out.println("World");
+			} else {
+				System.out.println(t);
+			}
+			
+		}
 	}
+	
 
 }
