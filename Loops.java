190d4a04f3b4fff1fbaf95d20079c2b15ae1098d
jeff1haupt <66330433+jeff1haupt@users.noreply.github.com>
2020-06-13 18:35:07 -0400

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,16 @@
+
+public class Loops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 0;
+		while(i <= 100) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+			i++;
+		}
+	}
+
+}
